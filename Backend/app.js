5b93874e76abfbea4786841f10211b07ceb99393
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-09-08 17:32:31 +0100

    updating my app.js route to have an auth route

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { PORT } from "./config/env.config.js";
 import connectToDatabase from "./database/mongo.database.js";
+import authRouter from "./router/auth.router.js";
 
 const app = express();
 connectToDatabase();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/auth", authRouter);
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
