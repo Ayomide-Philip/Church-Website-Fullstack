714d2e52ec8567305bd613d65db23adb64bc5d3b
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-09-09 20:19:15 +0100

    renaiming the file

--- Backend/router/leaders.router.js
+++ Backend/router/leaders.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createLeader } from "../controller/leaders.controllers.js";
+import { createLeader } from "../controller/leaders.controller.js";
 
 const leaderRouter = new Router();
 
