5103bc9950c2b3e18260e6be46e623e71e4c5a21
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-09-09 17:54:35 +0100

    removing unused import and deleting uncessary files

--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -1,4 +1,3 @@
-import Header from "../componets/header";
 import Cta from "../componets/home/cta";
 import Explore from "../componets/home/explore";
 import Faq from "../componets/home/faq";
@@ -6,7 +5,6 @@ import Team from "../componets/home/leaders";
 import Step from "../componets/home/steps";
 import VideoBanner from "../componets/videoBanner";
 import Contact from "../componets/home/contact";
-import Footer from "../componets/footer";
 export default function Home() {
   return (
     <div>
