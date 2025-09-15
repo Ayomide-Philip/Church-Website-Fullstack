acd8a9c341ff0bf808220e2c9f531a1c85f4308e
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-09-15 21:14:17 +0100

    updating my login route and designing my login page

--- Frontend/src/router.jsx
+++ Frontend/src/router.jsx
@@ -4,6 +4,7 @@ import DashboardHome from "./pages/dashboard";
 import OurLeader from "./pages/home/leaders";
 import HomeLayout from "./componets/homeLayout";
 import DashboardLayout from "./componets/dashboard/dashboardLayout";
+import Login from "./pages/login";
 export default function PageRouter() {
   return (
     <RouterProvider
@@ -14,6 +15,7 @@ export default function PageRouter() {
           children: [
             { element: <Home />, index: true },
             { path: "/leaders", element: <OurLeader /> },
+            { path: "/login", element: <Login /> },
           ],
         },
         {
