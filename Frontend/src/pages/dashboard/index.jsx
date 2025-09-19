8399e44edd9e6a2118a305ed0a8c11ce098989d4
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-09-19 17:20:11 +0100

    updating my codebase

--- Frontend/src/pages/dashboard/index.jsx
+++ Frontend/src/pages/dashboard/index.jsx
@@ -1,82 +1,13 @@
-import { redirect, useLoaderData } from "react-router-dom";
+import { redirect } from "react-router-dom";
 import DashboardOverview from "../../componets/dashboard/home/overview";
 import DashboardQuickActions from "../../componets/dashboard/home/quickAction";
+import RecentLeaders from "../../componets/dashboard/home/recentLeaders";
 export default function DashboardHome() {
-  const { leaders } = useLoaderData();
-
   return (
     <>
       <DashboardOverview />
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-6">
-        <div className="lg:col-span-2 bg-white rounded-lg shadow overflow-hidden">
-          <div className="px-6 py-4 border-b border-gray-200">
-            <h2 className="text-lg font-semibold text-gray-800">
-              Recent Leaders
-            </h2>
-          </div>
-          <div className="overflow-x-auto">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-gray-50">
-                <tr>
-                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Name
-                  </th>
-                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Roles
-                  </th>
-                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Descriptions
-                  </th>
-                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Created At
-                  </th>
-                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Updated At
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200">
-                {leaders.map(
-                  ({ name, role, createdAt, description, updatedAt }, idx) => {
-                    return (
-                      <tr key={idx}>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          <div className="flex items-center">
-                            <div className="flex-shrink-0 h-10 w-10">
-                              <img
-                                className="h-10 w-10 rounded-full"
-                                src="/blank-profile-picture-973460_960_720.webp"
-                                alt={name}
-                              />
-                            </div>
-                            <div className="ml-4">
-                              <div className="text-sm font-medium text-gray-900">
-                                {name}
-                              </div>
-                            </div>
-                          </div>
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          <div className="text-sm text-gray-900">{role}</div>
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          {description}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {new Date(createdAt).toLocaleDateString()}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {new Date(updatedAt).toLocaleDateString()}
-                        </td>
-                      </tr>
-                    );
-                  }
-                )}
-              </tbody>
-            </table>
-          </div>
-        </div>
-
+        <RecentLeaders />
         <div className="bg-white rounded-lg shadow overflow-hidden">
           <div className="px-6 py-4 border-b border-gray-200">
             <h2 className="text-lg font-semibold text-gray-800">Room Status</h2>
