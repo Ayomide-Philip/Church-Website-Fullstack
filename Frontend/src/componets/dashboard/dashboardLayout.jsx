b8818f9c319877dda70413546e568f6eb2dd943d
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-09-19 17:13:28 +0100

    updating my codebase

--- Frontend/src/componets/dashboard/dashboardLayout.jsx
+++ Frontend/src/componets/dashboard/dashboardLayout.jsx
@@ -8,7 +8,7 @@ export default function DashboardLayout() {
       <div className="flex flex-col flex-1 overflow-hidden">
         <DashboardHeader />
 
-        <main className="flex-1 overflow-y-auto p-6 bg-gray-100">
+        <main className="flex-1 overflow-y-auto p-3 bg-gray-100">
           <Outlet />
         </main>
       </div>
--- Frontend/src/pages/dashboard/leaders.jsx
+++ Frontend/src/pages/dashboard/leaders.jsx
@@ -1,90 +1,99 @@
+import { Plus } from "lucide-react";
+
 export default function DashboardLeader() {
   return (
-    <div class="lg:col-span-2 bg-white rounded-lg shadow overflow-hidden">
-      <div class="px-6 py-4 border-b border-gray-200">
-        <h2 class="text-lg font-semibold text-gray-800">Recent Leaders</h2>
-      </div>
-      <div class="overflow-x-auto">
-        <table class="min-w-full divide-y divide-gray-200">
-          <thead class="bg-gray-50">
-            <tr>
-              <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Name
-              </th>
-              <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Roles
-              </th>
-              <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Descriptions
-              </th>
-              <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Created At
-              </th>
-              <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Updated At
-              </th>
-            </tr>
-          </thead>
-          <tbody class="bg-white divide-y divide-gray-200">
-            <tr>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="flex items-center">
-                  <div class="flex-shrink-0 h-10 w-10">
-                    <img
-                      class="h-10 w-10 rounded-full"
-                      alt="Areo Ayomide"
-                      src="/blank-profile-picture-973460_960_720.webp"
-                    />
-                  </div>
-                  <div class="ml-4">
-                    <div class="text-sm font-medium text-gray-900">
-                      Areo Ayomide
+    <>
+      <div className="lg:col-span-2 bg-white rounded-lg shadow overflow-hidden">
+        <div className="px-6 py-4 border-b border-gray-200">
+          <h2 className="text-lg font-semibold text-gray-800">
+            Recent Leaders
+          </h2>
+        </div>
+        <div className="overflow-x-auto">
+          <table className="min-w-full divide-y divide-gray-200">
+            <thead className="bg-gray-50">
+              <tr>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Name
+                </th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Roles
+                </th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Descriptions
+                </th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Created At
+                </th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Updated At
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              <tr>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0 h-10 w-10">
+                      <img
+                        className="h-10 w-10 rounded-full"
+                        alt="Areo Ayomide"
+                        src="/blank-profile-picture-973460_960_720.webp"
+                      />
+                    </div>
+                    <div className="ml-4">
+                      <div className="text-sm font-medium text-gray-900">
+                        Areo Ayomide
+                      </div>
                     </div>
                   </div>
-                </div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-gray-900">Admi</div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">hello world</td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                9/16/2025
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                9/16/2025
-              </td>
-            </tr>
-            <tr>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="flex items-center">
-                  <div class="flex-shrink-0 h-10 w-10">
-                    <img
-                      class="h-10 w-10 rounded-full"
-                      alt="Areo Ayomide"
-                      src="/blank-profile-picture-973460_960_720.webp"
-                    />
-                  </div>
-                  <div class="ml-4">
-                    <div class="text-sm font-medium text-gray-900">
-                      Areo Ayomide
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="text-sm text-gray-900">Admi</div>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">hello world</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  9/16/2025
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  9/16/2025
+                </td>
+              </tr>
+              <tr>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0 h-10 w-10">
+                      <img
+                        className="h-10 w-10 rounded-full"
+                        alt="Areo Ayomide"
+                        src="/blank-profile-picture-973460_960_720.webp"
+                      />
+                    </div>
+                    <div className="ml-4">
+                      <div className="text-sm font-medium text-gray-900">
+                        Areo Ayomide
+                      </div>
                     </div>
                   </div>
-                </div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-gray-900">Admin</div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">hello admin</td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                9/16/2025
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                9/16/2025
-              </td>
-            </tr>
-          </tbody>
-        </table>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="text-sm text-gray-900">Admin</div>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">hello admin</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  9/16/2025
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  9/16/2025
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+      <button className="flex fixed bottom-8 right-7 bg-blue-700 p-3 rounded-full">
+        <Plus color="white" />
+      </button>
+    </>
   );
 }
