96696b782592f664febfff7d2b1e04751935ce6c
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-09-27 17:32:17 +0100

    updating my banner

---
+++ Frontend/src/componets/home/blogImageBanner.jsx
@@ -0,0 +1,38 @@
+import { Search, X } from "lucide-react";
+import { useState } from "react";
+export default function BlogImageBanner() {
+  const [query, setQuery] = useState("");
+  return (
+    <div className="flex h-[70vh] bg-[url(/images/cta-image.jpg)] text-center justify-center items-center flex-col gap-5">
+      <p className="bg-blue-500 p-1 px-4 font-light text-white font-mono">
+        BLOG
+      </p>
+      <h1 className="text-4xl text-black md:text-white font-bold capitalize font-mono">
+        Explore the Latest Activities
+      </h1>
+      <div className="relative">
+        <input
+          type="text"
+          className="bg-white w-[250px] sm:w-[500px] h-12 rounded-2xl p-5 text-black placeholder:text-black focus:outline-none focus:border-blue-600 focus:border-2"
+          placeholder="Search for a particular topic"
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+        />
+        <div className="text-black absolute right-4 top-3 cursor-pointer">
+          <Search />
+        </div>
+        {query && (
+          <div className="text-black absolute right-12 top-3 cursor-pointer">
+            <X
+              onClick={() => {
+                setQuery("");
+              }}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
